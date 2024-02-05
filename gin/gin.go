3621e88cfd5820c0fd0ad60066edc226f47e0d0c
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-02-05 12:01:35 +0800

    Add files via upload

--- gin/gin.go
+++ gin/gin.go
@@ -1,23 +1,36 @@
 package main
 
 import (
-	// "github.com/gin-contrib/cors"
+	"golang/gin/app"
+
 	"github.com/gin-gonic/gin"
-)
+) // "github.com/gin-contrib/cors"
 
 func main() {
 	gin.SetMode(gin.DebugMode)
 
+	// 建立一個 Gin 引擎
 	router := gin.Default()
 
 	// 允許來自所有來源的 CORS 請求
 	// router.Use(cors.Default()) // 使用cors中介軟體
 
 	// 設定路由
-	router.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello GO!")
+	router.Any("/go", func(c *gin.Context) {
+		if c.Request.Method == "GET" {
+			c.String(200, "Hello GO!")
+		}
+	})
+
+	// 設定 app 路由
+	app.SetupRouter(router)
+
+	// 處理沒有定義的路徑
+	router.NoRoute(func(c *gin.Context) {
+		// 將請求轉發到埠 5002
+		c.Redirect(302, "http://127.0.0.1:5002")
 	})
 
 	// 監聽埠
-	router.Run("127.0.0.1:9090")
+	router.Run("127.0.0.1:5001")
 }
