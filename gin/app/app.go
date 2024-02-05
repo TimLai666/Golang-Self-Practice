788c5d9356bff4fa057c8e7a7030cebb11fdfe00
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-02-05 12:02:27 +0800

    Add files via upload

--- gin/app/app.go
+++ gin/app/app.go
@@ -1,1 +1,12 @@
+// app.go
 
+package app
+
+import "github.com/gin-gonic/gin"
+
+func SetupRouter(router *gin.Engine) {
+	// 設定 app 相關路由
+	router.GET("/app", func(c *gin.Context) {
+		c.String(200, "Hello app!")
+	})
+}
