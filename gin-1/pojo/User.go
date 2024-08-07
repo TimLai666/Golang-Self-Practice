3a89c2ce0f0a8aeec81f3a8a9bdd1630004c6329
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-08-07 13:17:19 +0800

    upload

--- gin-1/pojo/User.go
+++ gin-1/pojo/User.go
@@ -20,3 +20,19 @@ func FindUserById(id string) User {
 	database.DBconnect.Where("id = ?", id).First(&user)
 	return user
 }
+
+func AddUser(user User) User {
+	database.DBconnect.Create(&user)
+	return user
+}
+
+func DeleteUser(id string) bool {
+	user := User{}
+	result := database.DBconnect.Where("id = ?", id).Delete(&user).RowsAffected
+	return result != 0
+}
+
+func UpdateUser(userId string, user User) User {
+	database.DBconnect.Model(&user).Where("id = ?", userId).Updates(&user)
+	return user
+}
--- gin-1/service/UserService.go
+++ gin-1/service/UserService.go
@@ -4,12 +4,9 @@ import (
 	"gin-1/pojo"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
-
-var userList []pojo.User
 
 // GET User
 func FindAllUsers(c *gin.Context) {
@@ -37,38 +34,34 @@ func PostUser(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	userList = append(userList, user)
-	c.JSON(http.StatusOK, "User Added")
+	newUser := pojo.AddUser(user)
+	c.JSON(http.StatusOK, newUser)
 }
 
 // DELETE User
 func DeleteUser(c *gin.Context) {
-	id, _ := strconv.Atoi(c.Param("id"))
-	for _, user := range userList {
-		if user.Id == id {
-			userList = append(userList[:id], userList[id+1:]...)
-			c.JSON(http.StatusOK, "User Deleted")
-			return
-		}
+	userId := c.Param("id")
+	result := pojo.DeleteUser(userId)
+	if !result {
+		c.JSON(http.StatusNotFound, "User Not Found")
+		return
 	}
-	c.JSON(http.StatusNotFound, "User Not Found")
+	c.JSON(http.StatusOK, "User Deleted")
 }
 
 // PUT User
 func PutUser(c *gin.Context) {
-	beforeUser := pojo.User{}
-	err := c.BindJSON(&beforeUser)
+	userId := c.Param("id")
+	user := pojo.User{}
+	err := c.BindJSON(&user)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	userId, _ := strconv.Atoi(c.Param("id"))
-	for i, user := range userList {
-		if user.Id == userId {
-			userList[i] = beforeUser
-			c.JSON(http.StatusOK, "User Updated")
-			return
-		}
+	newUser := pojo.UpdateUser(userId, user)
+	if newUser.Id == 0 {
+		c.JSON(http.StatusNotFound, "User Not Found")
+		return
 	}
-	c.JSON(http.StatusNotFound, "User Not Found")
+	c.JSON(http.StatusOK, newUser)
 }
