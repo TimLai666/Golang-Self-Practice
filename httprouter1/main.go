1520b58532e95dea7750f219bdcf99fe296cb196
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-08-15 15:27:46 +0800

    Update main.go

--- httprouter1/main.go
+++ httprouter1/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "github.com/julienschmidt/httprouter"
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
 
 func main() {
 	router := httprouter.New()
+	router.GET("/default", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		w.Write([]byte("default GET"))
+	})
+
+	router.POST("/default", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		w.Write([]byte("default POST"))
+	})
+
+	// router.GET("/user/:name", func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// 	w.Write([]byte("user " + ps.ByName("name")))
+	// })
+
+	router.GET("/user/*name", func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		w.Write([]byte("user " + ps.ByName("name")))
+	})
+
+	http.ListenAndServe(":8080", router)
 }
