dffea5f376b199a934cd883c854c3ab8b009fd9c
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-08-16 12:46:29 +0800

    Create main.go

---
+++ httprouter3/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	router := httprouter.New()
+	router.serveFiles("/static/*filepath", http.Dir("./files"))
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
