3d7e4db83c3a63dc13810c8a742d2ad105c579c1
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-08-16 12:47:05 +0800

    Update main.go

--- httprouter3/main.go
+++ httprouter3/main.go
@@ -9,6 +9,6 @@ import (
 
 func main() {
 	router := httprouter.New()
-	router.serveFiles("/static/*filepath", http.Dir("./files"))
+	router.ServeFiles("/static/*filepath", http.Dir("./files"))
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
