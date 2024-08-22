427c489484d188d5e1b86fac451b3f5fa2c72a51
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-08-22 17:48:23 +0800

    upload

--- mongodb-bson/go.mod
+++ mongodb-bson/go.mod
@@ -1,3 +1,5 @@
 module mongodb-bson
 
 go 1.21.6
+
+require go.mongodb.org/mongo-driver v1.16.1
---
+++ mongodb-bson/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func main() {
+	testM := bson.M{
+		"jobName": "job1",
+	}
+
+	var raw bson.Raw
+	tmp, _ := bson.Marshal(testM)
+	bson.Unmarshal(tmp, &raw)
+
+	fmt.Println(testM)
+	fmt.Println(raw)
+}
